641e27a4604bcedca303afc1cbd331524f1b088e
Gaitano <gaitano.george@student.moringaschool.com>
2024-03-18 15:45:20 +0300

    Seeded the data and edited the models

--- server/app.py
+++ server/app.py
@@ -5,11 +5,10 @@ from flask_restx import Api, fields, Namespace, Resource
 
 from models import db, app
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SECRET_KEY'] = 'bb8f7de46cd4426ebf5ca7df06d43665'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)
+# db.init_app(app)
 migrate = Migrate(app, db)
 api = Api(app)
 
